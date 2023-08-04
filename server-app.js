6190e8ceac879eeb1bb831ad9c9ccac85b5bf004
vamsi <rvamsi599@gmail.com>
2023-08-04 09:24:04 +0530

    node app with mysql backend

--- server-app.js
+++ server-app.js
@@ -1,31 +1,53 @@
 const express = require("express")
 const app = express()
-const mongoose = require('mongoose');
+var mysql = require('mysql');
+const bodyParser = require('body-parser')
 
-const mongodb = "mongodb://127.0.0.1:27017/School";
-mongoose.connect(mongodb).then(() => console.log("Connected!")).catch((err) => console.log(err));
-
-const studentSchema = new mongoose.Schema({
-	name: String,
+var con = mysql.createConnection({
+	host: "node-sql.ceqxw7fztytm.us-west-1.rds.amazonaws.com",
+	user: "admin",
+	password: "vamsiadmin",
+	multipleStatements: true
 });
-const Student = mongoose.model('Student', studentSchema);
 
-app.get('/insert', (req, res) => {
-	var stud = new Student({ name: req.query.name })
-	stud.save()
-	res.redirect('http://localhost:8888/')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+con.connect(function (err) {
+	if (err) throw err;
+	console.log("Connected!");
+	con.query("CREATE DATABASE IF NOT EXISTS Student; USE Student;", function (err, result) {
+		if (err) throw err;
+		console.log("Database created");
+	});
+	var ctable = "CREATE TABLE IF NOT EXISTS Persons (Personid INT NOT NULL AUTO_INCREMENT, Name VARCHAR(255), PRIMARY KEY (Personid));"
+	con.query(ctable, function (err, result) {
+		if (err) throw err;
+		console.log("Table created");
+	});
+});
+
+app.post('/insert', (req, res) => {
+	con.query(`INSERT INTO Persons(Name) VALUES (${JSON.stringify(req.body.name)});`, function (err, result) {
+		if (err) throw err;
+		console.log("inserted")
+		res.redirect(req.get('referer'))
+	})
 })
 
-app.get('/delete', async (req, res) => {
-	var sid = req.query.studentId
-	console.log(sid)
-	await Student.deleteOne({ _id: sid })
-	res.redirect('http://localhost:8888/')
+app.post('/delete', async (req, res) => {
+	var sid = req.body.studentId
+	con.query(`DELETE FROM Persons WHERE Personid = ${sid}`, function (err, data) {
+		if (err) throw err;
+		console.log("deleted")
+		res.redirect(req.get('referer'))
+	});
 })
 
-app.get('/slist', async (req, res) => {
-	var slist = await Student.find({})
-	res.json(slist)
+app.post('/slist', (req, res) => {
+	con.query("SELECT * FROM Persons", function (err, data) {
+		if (err) throw err;
+		res.json(data)
+	});
 })
 
 app.listen(8080, () => console.log("Listening ...."))
--- server-front.js
+++ server-front.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const axios = require("axios")
 const app = express()
+const apiServ = "http://localhost:8080"
 
 app.set('view engine', 'ejs')
 
 app.get('/', async (req, res) => {
-	var studs = await axios.get("http://localhost:8080/slist");
-	console.log(studs.data)
-	res.render('index', { students: studs.data })
+	var studs = await axios.post("http://localhost:8080/slist");
+	res.render('index', { students: studs.data, apie: apiServ })
 })
 
 app.listen(8888, () => console.log("Listening ...."))
